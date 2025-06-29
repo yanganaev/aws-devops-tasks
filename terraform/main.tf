4b773749244705ad96f0248acb519f47ca3cc941
Felix Yanganaev <yangfell@gmail.com>
2025-06-29 21:47:48 +0600

    add terraform

---
+++ terraform/main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "${var.project_name}-bucket-${random_id.bucket_id.hex}"
+  force_destroy = true
+
+  tags = {
+    Name        = "Example S3 Bucket"
+    Environment = var.environment
+  }
+}
+
+resource "random_id" "bucket_id" {
+  byte_length = 4
+}
