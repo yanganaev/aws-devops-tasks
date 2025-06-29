f946e8fef1395a1f576c13e4c245ab4e1caee592
Felix Yanganaev <yangfell@gmail.com>
2025-06-29 21:58:43 +0600

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,10 @@ provider "aws" {
   region = var.aws_region
 }
 
+resource "random_id" "bucket_id" {
+  byte_length = 4
+}
+
 resource "aws_s3_bucket" "example" {
   bucket        = "${var.project_name}-bucket-${random_id.bucket_id.hex}"
   force_destroy = true
@@ -10,8 +14,4 @@ resource "aws_s3_bucket" "example" {
     Name        = "Example S3 Bucket"
     Environment = var.environment
   }
-}
-
-resource "random_id" "bucket_id" {
-  byte_length = 4
 }
