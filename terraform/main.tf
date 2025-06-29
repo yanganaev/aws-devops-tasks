286f0b11778aed807914a898076184ed93cac4a1
Felix Yanganaev <yangfell@gmail.com>
2025-06-29 21:55:32 +0600

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "example" {
-  bucket = "${var.project_name}-bucket-${random_id.bucket_id.hex}"
+  bucket        = "${var.project_name}-bucket-${random_id.bucket_id.hex}"
   force_destroy = true
 
   tags = {
